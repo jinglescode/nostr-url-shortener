cd5a8f1cca158988cd40402b547bea0c35ee2f15
Hong Jing (Jingles) <jinglescode@gmail.com>
2024-11-26 09:52:14 +0800

    add try catch to check urls

--- src/utils/checkUrlWebRiskSafe.ts
+++ src/utils/checkUrlWebRiskSafe.ts
@@ -1,14 +1,19 @@
 export async function checkUrlWebRiskSafe(url: string) {
-  const res = await fetch(
-    `https://webrisk.googleapis.com/v1/uris:search?threatTypes=MALWARE&threatTypes=SOCIAL_ENGINEERING&threatTypes=UNWANTED_SOFTWARE&threatTypes=SOCIAL_ENGINEERING_EXTENDED_COVERAGE&key=${process.env.NEXT_PUBLIC_WEBRISKAPI}&uri=${url}`
-  );
+  try {
+    const res = await fetch(
+      `https://webrisk.googleapis.com/v1/uris:search?threatTypes=MALWARE&threatTypes=SOCIAL_ENGINEERING&threatTypes=UNWANTED_SOFTWARE&threatTypes=SOCIAL_ENGINEERING_EXTENDED_COVERAGE&key=${process.env.NEXT_PUBLIC_WEBRISKAPI}&uri=${url}`
+    );
 
-  const data = await res.json();
+    const data = await res.json();
 
-  if (Object.keys(data).length === 0) {
-    return true;
+    if (Object.keys(data).length === 0) {
+      return true;
+    }
+
+    console.log("data", data);
+    return false;
+  } catch (e) {
+    console.log(e);
+    return false;
   }
-
-  console.log("data", data);
-  return false;
 }
